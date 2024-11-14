23cd45bc8c063a4f16d8a209f0b10c917987b128
vakhtangi1 <s054961@ad.viko.lt>
2024-11-14 16:47:26 +0000

    Added application.properties to the resource package. Added Swagger dependencies

---
+++ src/main/java/team15/homelessapp/HomelessRestApplication.java
@@ -0,0 +1,12 @@
+package team15.homelessapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomelessRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomelessRestApplication.class, args);
+    }
+}
